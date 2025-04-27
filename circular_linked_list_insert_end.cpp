58a46808bd62add6de8faefbeda3b079b1ad343a
vijayKota2776 <vijaykota2776@gmail.com>
2025-04-27 23:16:18 +0530

    add code file

---
+++ circular_linked_list_insert_end.cpp
@@ -0,0 +1,55 @@
+// Create circular linked list data structure & perform insert at end and display operations.
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+};
+
+class CircularLinkedList {
+    Node* last;
+public:
+    CircularLinkedList() {
+        last = nullptr;
+    }
+
+    void insertAtEnd(int val) {
+        Node* newNode = new Node(val);
+        if (last == nullptr) {
+            last = newNode;
+            last->next = last;
+        } else {
+            newNode->next = last->next;
+            last->next = newNode;
+            last = newNode;
+        }
+    }
+
+    void display() {
+        if (last == nullptr) {
+            cout << "List is Empty" << endl;
+            return;
+        }
+        Node* temp = last->next;
+        do {
+            cout << temp->data << " ";
+            temp = temp->next;
+        } while (temp != last->next);
+        cout << endl;
+    }
+};
+
+int main() {
+    CircularLinkedList list;
+    list.insertAtEnd(10);
+    list.insertAtEnd(20);
+    list.insertAtEnd(30);
+    list.display();
+    return 0;
+}
