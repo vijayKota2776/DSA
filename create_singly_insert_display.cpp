40754e7cee5db41128d87897ac2f8abe057ffe9d
vijayKota2776 <vijaykota2776@gmail.com>
2025-04-29 22:27:01 +0530

    add code file

---
+++ create_singly_insert_display.cpp
@@ -0,0 +1,52 @@
+// Create Singly linked list data structure & perform insert at end and display operations.
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+};
+
+class SinglyLinkedList {
+    Node* head;
+public:
+    SinglyLinkedList() {
+        head = nullptr;
+    }
+
+    void insertAtEnd(int val) {
+        Node* newNode = new Node(val);
+        if (head == nullptr) {
+            head = newNode;
+            return;
+        }
+        Node* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+
+    void display() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    SinglyLinkedList list;
+    list.insertAtEnd(10);
+    list.insertAtEnd(20);
+    list.insertAtEnd(30);
+    list.display();
+    return 0;
+}
