fe9a90cabbe3e2a168c4bdfaef7998103a29090d
vijayKota2776 <vijaykota2776@gmail.com>
2025-04-30 23:22:05 +0530

    add code file

---
+++ double_linked_list_insert_end.cpp
@@ -0,0 +1,55 @@
+// Create Doubly linked list data structure & perform insert at end and display operations.
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* prev;
+    Node* next;
+    Node(int val) {
+        data = val;
+        prev = nullptr;
+        next = nullptr;
+    }
+};
+
+class DoublyLinkedList {
+    Node* head;
+public:
+    DoublyLinkedList() {
+        head = nullptr;
+    }
+
+    void insertAtEnd(int val) {
+        Node* newNode = new Node(val);
+        if (head == nullptr) {
+            head = newNode;
+            return;
+        }
+        Node* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        newNode->prev = temp;
+    }
+
+    void display() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    DoublyLinkedList list;
+    list.insertAtEnd(10);
+    list.insertAtEnd(20);
+    list.insertAtEnd(30);
+    list.display();
+    return 0;
+}
